504019697f6197e51ed6c16167a530a13dcaa065
prince-7 <amanvijaykumarsingh7792@gmail.com>
2020-05-27 04:46:39 +0530

    Updates

---
+++ precache-manifest.3f4ae9d0f6df92afbe76ea3a36898412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7d961a19151f449216d8492bd7cc8d",
+    "url": "/MoviezBin/index.html"
+  },
+  {
+    "revision": "de6dee85c12a18599562",
+    "url": "/MoviezBin/static/css/2.d811125f.chunk.css"
+  },
+  {
+    "revision": "bea04e56bfecba93976f",
+    "url": "/MoviezBin/static/css/main.e4745f99.chunk.css"
+  },
+  {
+    "revision": "de6dee85c12a18599562",
+    "url": "/MoviezBin/static/js/2.ed28d22c.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/MoviezBin/static/js/2.ed28d22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea04e56bfecba93976f",
+    "url": "/MoviezBin/static/js/main.2e27ef9f.chunk.js"
+  },
+  {
+    "revision": "e9d6d0a06c420a16757a",
+    "url": "/MoviezBin/static/js/runtime-main.ba7bdfc2.js"
+  }
+]);
