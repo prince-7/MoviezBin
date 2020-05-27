0d35fb176521f25102db2b6d0b8142e5a6edd0e3
Aman Singh <53997924+prince-7@users.noreply.github.com>
2020-05-27 16:09:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Homepage from './Homepage';
 import Result from './Result';
 import Watchlist from './Watchlist';
 import {Route} from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom';
 
 const App = () => {
 	return(
 		<div>
+		<BrowserRouter basename="/MoviezBin">
 			<Route exact path = "/Homepage" component={Homepage} />
 			<Route exact path = "/Result" component={Result} />
 			<Route exact path ="/Watchlist" component={Watchlist}/>
+		</BrowserRouter>
 		</div>
 	);
 }
